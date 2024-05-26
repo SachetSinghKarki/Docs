4b1e2ba888abbdee5629ff7ab50723f8bae4d9ac
SachetSinghKarki <146599855+SachetSinghKarki@users.noreply.github.com>
2024-05-26 14:28:56 +0545

    Update Background.jsx

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -5,7 +5,7 @@ const Background = () => {
     <>
     <div className="fixed z-[2] w-full h-screen">
 
-     <div className='absolute top-[5%] w-full py-10 flex justify-center tezt-zinc-600 font-semibold text-xl'>Documents.</div>
+     <div className='absolute top-[5%] w-full py-10 flex justify-center text-zinc-200 font-semibold text-xl'>Sachet Singh Karki.</div>
 
      <h1 className= 'absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[15vw] leading-none tracking-tight font-semibold text-zinc-900 '>Docs.</h1>
      
